3a496bb40e349df6f5ab155ebe1ee510c724b153
James Zhao <jzha0385@uni.sydney.edu.au>
2023-11-25 15:54:18 +1100

    Note with obsidian added in

---
+++ 2023Nov/takeinput.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string str;
+    cout << "May I have your name?";
+    cin >> str;
+    cout << "Hi! " << str << "!" << endl;
+    return(0);
+}
---
+++ 2023Nov/takeinput.h
@@ -0,0 +1,14 @@
+//
+// Created by James Yi Jie Zhao on 24/11/2023.
+//
+
+#ifndef CPP_STUDY_NOTE_TAKEINPUT_H
+#define CPP_STUDY_NOTE_TAKEINPUT_H
+
+
+class takeinput {
+
+};
+
+
+#endif //CPP_STUDY_NOTE_TAKEINPUT_H
