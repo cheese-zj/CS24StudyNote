876812b034404d2b98af53fe816bef63099f5ee5
miba9209 <your_email@example.com>
2024-09-27 10:57:03 +1000

    9.27 Regular Update

--- LangChain_Project/SQL/TestA/Averge_volume.sql
+++
@@ -1,9 +0,0 @@
-USE stock_analysis;
-
-
-SELECT AVG(volume)
-FROM stock
-WHERE 
-NOT volume is NULL 
-AND
-stock_name='恒生电子'
--- LangChain_Project/SQL/TestA/Delete_specific_rows.sql
+++
@@ -1,8 +0,0 @@
-USE stock_analysis;
-
-SET SQL_SAFE_UPDATES = 0;
- 
-DELETE FROM stock WHERE date='2023-12-01';
-DELETE FROM stock WHERE date='2023-12-11' AND stock_name='顶点软件';
-
-SET SQL_SAFE_UPDATES = 1;
